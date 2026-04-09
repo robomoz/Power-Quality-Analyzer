80235246c162783707b68b832fd89cf213604d93
robomozz <mollie2.Bennington@live.uwe.ac.uk>
2026-04-09 17:12:38 +0100

    defining the stuct from the data

---
+++ waveform.c
@@ -0,0 +1,4 @@
+//
+// Created by mm2-bennington on 09/04/2026.
+//
+
---
+++ waveform.h
@@ -0,0 +1,19 @@
+
+#ifndef PROGRAMMINGCOURSEWORK_WAVEFORM_H
+#define PROGRAMMINGCOURSEWORK_WAVEFORM_H
+
+//1 - define the stuct
+
+typedef struct {
+    double timestamp;
+    double phase_A_voltage;
+    double phase_B_voltage;
+    double phase_C_voltage;
+    double line_current;
+    double frequency;
+    double power_factor;
+    double thd_percent;
+
+} waveformSample;
+
+#endif //PROGRAMMINGCOURSEWORK_WAVEFORM_H
