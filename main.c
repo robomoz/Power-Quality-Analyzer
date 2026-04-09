8c7649956007715f9a845d1a539319dc4814fd35
robomozz <mollie2.Bennington@live.uwe.ac.uk>
2026-04-09 17:43:01 +0100

    defining the stuct from the data

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Waveform.h"
+#include"io.h"
+//memory allocation
+int main() {
+    int num_rows = 1000;
+    WaveformSample *data = (WaveformSample *)malloc(num_rows * sizeof(WaveformSample));
+
+    if (data == NULL){
+        printf("Memory allocation failed! \n ");
+        return 1;
+    }
+
+    //loading function
+    if (load_data("power_quality_log.csv", data, num_rows) != 0){
+        printf("Error loading file!\n");
+        free(data);  //clearing data
+        return 1;
+    }
+
+    printf("Successfully loaded %d rows of data. \n", num_rows);
+    free(data);
+    return 0;
+}
