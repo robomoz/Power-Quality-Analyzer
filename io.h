b82cca2c3dfa11e978dd38902c5198350d8a44e0
robomozz <mollie2.Bennington@live.uwe.ac.uk>
2026-04-10 16:43:12 +0100

    io.c - moves data

--- io.h
+++ io.h
@@ -8,5 +8,5 @@
 //just to load data from the csv
 int load_data(const char *filename, WaveformSample *data, int num_rows);
 
-void export_report(cont char *filename, double rmsA, double rmsB, double rmsC);
+void export_report(cont char *filename, double rmsA, double rmsB, double rmsC); //root mean square for phase a,b,c
 #endif //PROGRAMMINGCOURSEWORK_IO_H
