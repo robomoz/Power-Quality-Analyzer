3a4fd984b30d68aa9160d2832b9782312bc0a142
robomozz <mollie2.Bennington@live.uwe.ac.uk>
2026-04-22 14:53:04 +0100

    define the stuct

--- io.h
+++ io.h
@@ -7,6 +7,3 @@
 
 //just to load data from the csv
 int load_data(const char *filename, WaveformSample *data, int num_rows);
-
-void export_report(cont char *filename, double rmsA, double rmsB, double rmsC);
-#endif //PROGRAMMINGCOURSEWORK_IO_H
