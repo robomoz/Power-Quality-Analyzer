5ec8e411e733378b31dbb9b8fa83477c7f1a5421
robomozz <mollie2.Bennington@live.uwe.ac.uk>
2026-04-22 15:17:31 +0100

    define struct

--- waveform.h
+++ waveform.h
@@ -6,15 +6,15 @@
 
 typedef struct {
     double timestamp;
-    double phase_A_voltage;
-    double phase_B_voltage;
-    double phase_C_voltage;
-    double line_current;
+    double phaseA;
+    double phaseB;
+    double phaseC;
+    double current;
     double frequency;
-    double power_factor;
-    double thd_percent;
+    double powerFactor;
+    double thd;
 
 } WaveformSample;
-double calculate_rms(WaveformSample *data, int num_rows, int phase_choice);
+
 
 #endif //PROGRAMMINGCOURSEWORK_WAVEFORM_H
